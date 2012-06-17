bf1474b7aed402bf0e772bafc23e9a7fd7625b63
Yufei Liu <spaceshiplyf@gmail.com>
2012-06-17 16:06:16 +0000

    add controller say hellp

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,4 @@
 class WelcomeController < ApplicationController
-  def index
+  def haha
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RailsinstallerDemo::Application.routes.draw do
-  get "welcome/index"
+  get "say/hello"
+
+  get "welcome/haha"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +52,7 @@ RailsinstallerDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'welcome#haha'
 
   # See how all your routes lay out with "rake routes"
 
